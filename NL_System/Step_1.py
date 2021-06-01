01e673e73dccde3545d9a387e4d90c432e0cb182
BunkerBuster <leesehyuno2@gmail.com>
2021-06-01 11:50:02 +0900

    step_1

--- NL_System/Step_1.py
+++ NL_System/Step_1.py
@@ -1,7 +1,10 @@
 # 암막, 자연광 색온도, 실시간, cas, 필요조도, 색온도 재현
 import pandas as pd
+from multiprocessing import Process, Queue
 from MongoDB import Load_MongoDB as LMDB
 from NL_System import Base_Process as bp
+
+
 def load_NL_CCT_mongo():
     step_data = LMDB.load_last1_cct()
     step_df = LMDB.mongodb_to_df(step_data, 'mongo_cas')
@@ -9,11 +12,14 @@ def load_NL_CCT_mongo():
     return step_df
 
 def process():
-    bp.process()
+    base = Process(target=bp.process)
+    base.start()
 
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     mongo_df = load_NL_CCT_mongo()
     cct_now = mongo_df['CCT'].values[0]
     print(cct_now)
+
 
 if __name__ == '__main__':
     process()

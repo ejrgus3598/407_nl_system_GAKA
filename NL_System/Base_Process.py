a3bd61841f284f3cea85e062f3e9d694ab727d22
BunkerBuster <leesehyuno2@gmail.com>
2021-06-01 10:25:15 +0900

    step_1

--- NL_System/Base_Process.py
+++ NL_System/Base_Process.py
@@ -3,14 +3,14 @@
 # 3. 릴레이 한번 껏다 켜기.
 # 4. 알고리즘 시작(이건 따로 상위에서 돌림)
 import threading, time
-from API import test
+from API import TCP_Server
 from Core import switch
 from Core import sensing_process as sp
 from multiprocessing import Process, Queue
 
 
 def process():
-    api = Process(target=test.process)
+    api = Process(target=TCP_Server.process)
     api.start()
 
     # switch.onnoff()
--- NL_System/Step_1.py
+++ NL_System/Step_1.py
@@ -1,1 +1,19 @@
 # 암막, 자연광 색온도, 실시간, cas, 필요조도, 색온도 재현
+import pandas as pd
+from MongoDB import Load_MongoDB as LMDB
+from NL_System import Base_Process as bp
+def load_NL_CCT_mongo():
+    step_data = LMDB.load_last1_cct()
+    step_df = LMDB.mongodb_to_df(step_data, 'mongo_cas')
+    step_df = step_df.reset_index(drop=True)
+    return step_df
+
+def process():
+    bp.process()
+
+    mongo_df = load_NL_CCT_mongo()
+    cct_now = mongo_df['CCT'].values[0]
+    print(cct_now)
+
+if __name__ == '__main__':
+    process()
